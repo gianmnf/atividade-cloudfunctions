10895fc9b4b0bc78148fb329230ff47021219316
Gian Michel <gianmnf@unipam.edu.br>
2020-10-04 13:47:29 -0300

    Função de inserir adicionada.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def insertCarros(request):
+from google.cloud import firestore
+
+def insertCar(request):
     request_json = request.get_json()
     def getAttribute(attr):
         if request.args and attr in request.args:
@@ -7,9 +9,26 @@ def insertCarros(request):
             return request_json[attr]
         else:
             return null
-    placa = getAttribute('placa')
-    cor = getAttribute('cor')
-    preco = getAttribute('preco')
-    modelo = getAttribute('modelo')
-    marca = getAttribute('marca')
-    return('Placa:' + placa + '\nCor:' + cor + '\nPreço:' + preco + '\nModelo:' + modelo + '\nMarca:' + marca) 
+    plate = getAttribute('plate')
+    color = getAttribute('color')
+    price = getAttribute('price')
+    model = getAttribute('model')
+    brand = getAttribute('brand')
+
+    db = firestore.Client()
+
+    car_ref = db.collection('cars').document(plate)
+
+    car = car_ref.get()
+
+    if car.exists:
+        return 'This car already exists in the database!'
+    else:
+        car_ref.set({
+            'plate': plate,
+            'color': color,
+            'price': price,
+            'model': model,
+            'brand': brand
+        })
+        return 'The car with the plate ' + plate + ' was added successfully.'
